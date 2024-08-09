52ba7c1f289a99b07538e28b003c8d97be2f582f
Roshan Santhosh <roshan.santhosh@gmail.com>
2024-08-09 07:39:44 -0700

    Cleaning up repo

--- test.py
+++
@@ -1,9 +0,0 @@
-from agents.workflow import *
-
-english_agent, spanish_agent, french_agent = generate_agents() 
-
-english_agent.send_text("Hello World!")
-
-english_agent.display_chat_history()
-
-spanish_agent.display_chat_history()
